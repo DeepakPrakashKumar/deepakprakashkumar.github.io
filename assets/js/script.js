4af09866c32f3b659cde4a2ec2279d95ab1c0b94
Deepak <deepakprakash1997@gmail.com>
2024-12-25 22:06:45 +0530

    Updated about section

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Path Planning", "Autonomous Applications", "Task Allocation", "Robotics"],
+    strings: ["Autonomous Vehicle Applications", "Path Planning", "Task Allocation", "Robotics"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
